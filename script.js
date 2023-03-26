378fc8b4f6c66ee067f5d788bd331bae1767b958
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2023-03-26 22:24:25 +0500

    Files uploaded

---
+++ script.js
@@ -0,0 +1,15 @@
+const endpoint = 'https://api.quotable.io/random'; // API endpoint for random quotes
+
+// Function to fetch quotes from the API and update the quote and author elements in the DOM
+async function getQuote() {
+  const response = await fetch(endpoint);
+  const { content, author } = await response.json();
+  document.getElementById('quote').textContent = content;
+  document.getElementById('author').textContent = `— ${author}`;
+}
+
+// Add an event listener to the new quote button to fetch a new quote and update the DOM
+document.getElementById('new-quote').addEventListener('click', getQuote);
+
+// Fetch a new quote on page load
+getQuote();
